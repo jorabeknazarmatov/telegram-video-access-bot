fd8a49c4fe38916682cb7d7034ce5245cd8499c6
TURABEK NAZARMATOV <7916947090@my.lerna.uz>
2025-08-15 13:52:42 +0500

    modified

--- handlers/get_posts.py
+++ handlers/get_posts.py
@@ -35,7 +35,7 @@ async def add_posts(callback: CallbackQuery):
                         f"🕒 Davomiyligi: {movie['duration'] // 60} soat {movie['duration'] % 60} daqiqa\n\n"
                         f"{movie['desc']}\n\n"
                         f"{' | '.join(f'#{cat}' for cat in json.loads(movie['category']))}\n\n"
-                        f"🎁 Tomosha qilish uchun quyidagi maxfiy kodni botga yuboring: <code>{movie['key']}</code>\n\n"
+                        f"🎁 Film kodi: <code>{movie['key']}</code>\n\n"
                         f"➡️ 🎬 {html.link('Ko‘rish', 'https://t.me/vaultfilmbot')}\n"
                         f"📌 Kanalga obuna bo‘ling: @Premium_Kino_Club"
                     ),
